1245417ca3417aa43c1042ea92fda4d7a5957c8c
JavaScript <61521224+Bearofbusines@users.noreply.github.com>
2022-05-17 08:50:45 -0400

    Update game.js

--- puzzlegame/game.js
+++ puzzlegame/game.js
@@ -18,9 +18,11 @@ loadRoot("assets/");
 //loadSprite("Emy", "sprites/Enemy.png");
 loadSprite("airb","sprites/Box.png");
 loadSprite("box","sprites/Box2.png");
-loadSprite("box","sprites/Box2.png");
-loadSprite("bean","sprites/bean.png");
+loadSprite("box2","sprites/Boxbrown.png");
+loadSprite("rules","sprites/For_Nongamer_'People'.png");
 loadSprite("Pbox","sprites/PlayerBox.png");
+loadSprite("Gbox","sprites/GoalBox.png");
+loadSound("score","sounds/score.mp3");
 
 
 function centerOffSet(x, y){
@@ -57,86 +59,217 @@ function addButton(txt, p, f) {
 	})
 
 }
-scene("game", ({levelid}) => {
+scene("game", ({levelid,bonus}) => {
 	const LEVELS = [
-	[
-		"===========================",
-		"=                g     ggg=",
-		"=                         =",
-		"= f                       =",
-		"=      b   b   b b        =",
-		"=                     =   =",
-		"= P      ====         =   =",
-		"=                     =   =",
-		"=        b     =      =   =",
-		"===========================",
-	],
-	]
+		[
+			"====",
+			"=p =",
+			"= f=",
+			"====",
+		],
+		[
+			"=========",
+			"=====  ==",
+			"=p    bf=",
+			"====== ==",
+			"=========",
+		],
+		[
+			"========",
+			"=fp b g=",
+			"========",
+		],
+		[
+			"==================",
+			"========  ========",
+			"====  == b========",
+			"==== b== b===  ===",
+			"=p   b   b      f=",
+			"=====g===g====g===",
+			"=====g===g========",
+			"==================",
+		],
+		[
+			"=======",
+			"=p g f=",
+			"=  b  =",
+			"====  =",
+			"=======",
+		],
+		
+		[
+			"====================",
+			"=p      b   b    gg=",
+			"=f        b b    gg=",
+			"====================",
+		],
+		[
+			"================",
+			"=p  b b bb gggg=",
+			"=f             =",
+			"================",
 
-	const lvl = addLevel(LEVELS[levelid || 0],{
-		width: 100,
-		height: 100,
-		"P": () => [
-			sprite("Pbox"),
-			color(RED),
-			z(2),
-			"player",
-			area(),
-			solid(),
-			{
-				oldpos: vec2(0,0),
-			},
 		],
-		"=": () => [
-			sprite("box"),
-			area(),
-			z(2),
-			solid(),
-			color(67,70,75),
-			"wall",
+		[
+			"========================",
+			"=p  ===g  b     =   b g=",
+			"=== ========= = = = == =",
+			"=   ==      = = = = == =",
+			"=b  == ==== === = = ====",
+			"=g=    ===        =   f=",
+			"========================",
 		],
-		"b": () => [
-			sprite("box"),
-			pos(),
-			area(),
-			color(101,56,24),
-			outline(100,[0,0,0]),
-			z(2),
-			solid(),
-			"boxx",
-			"Moveable",
+				[
+			"========================",
+			"=p  ===g        =     g=",
+			"=== =======b=b= = = = b=",
+			"=   ==      =g= = = =  =",
+			"=b  == ==== === = = ====",
+			"=g=    ===        =   f=",
+			"========================",
 		],
-		"g": () => [
-			sprite("box"),
-			area(),
-			pos(),
-			color(256,256,256),
-			z(1),
-			"goal",
-			{
-				Trigger: false
-			},
-		],
-		"f": () => [
-			sprite("Pbox"),
-			area(),
-			pos(),
-			color(256,256,256),
-			z(1),
-			"pgoal",
-			{
-				Trigger: false
-			},
-		],
-		" ": () => [
-			sprite("airb"),
-			area(),
-			pos(),
-			color(136,139,141,),
-			z(0),
-			"air",
-		],
-	});
+	]
+	const bonuns = [
+		"=====",
+		"=p=g=",
+		"= = =",
+		"= b =",
+		"= = =",
+		"= = =",
+		"= = =",
+		"= ===",
+		"=  f=",
+		"=====",
+	];
+	if(!bonus){
+		const lvl = addLevel(LEVELS[levelid || 0],{
+			width: 100,
+			height: 100,
+			"p": () => [
+				sprite("Pbox"),
+				color(RED),
+				z(2),
+				"player",
+				area(),
+				solid(),
+				{
+					oldpos: vec2(0,0),
+				},
+			],
+			"=": () => [
+				sprite("box"),
+				area(),
+				z(2),
+				solid(),
+				color(67,70,75),
+				"wall",
+			],
+			"b": () => [
+				sprite("box2"),
+				pos(),
+				area(),
+				//color(101,56,24),
+				z(2),
+				solid(),
+				"boxx",
+				"Moveable",
+			],
+			"g": () => [
+				sprite("Gbox"),
+				area(),
+				pos(),
+				color(256,256,256),
+				z(1),
+				"goal",
+				{
+					Trigger: false
+				},
+			],
+			"f": () => [
+				sprite("Pbox"),
+				area(),
+				pos(),
+				color(256,256,256),
+				z(1),
+				"pgoal",
+				{
+					Trigger: false
+				},
+			],
+			" ": () => [
+				sprite("airb"),
+				area(),
+				pos(),
+				color(136,139,141,),
+				z(0),
+				"air",
+			],
+		});
+	}else{
+		const lvl = addLevel(bonuns,{
+			width: 100,
+			height: 100,
+			"p": () => [
+				sprite("Pbox"),
+				color(RED),
+				z(2),
+				"player",
+				area(),
+				solid(),
+				{
+					oldpos: vec2(0,0),
+				},
+			],
+			"=": () => [
+				sprite("box"),
+				area(),
+				z(2),
+				solid(),
+				color(67,70,75),
+				"wall",
+			],
+			"b": () => [
+				sprite("box2"),
+				pos(),
+				area(),
+				//color(101,56,24),
+				z(2),
+				solid(),
+				"boxx",
+				"Moveable",
+			],
+			"g": () => [
+				sprite("Gbox"),
+				area(),
+				pos(),
+				color(256,256,256),
+				z(1),
+				"goal",
+				{
+					Trigger: false
+				},
+			],
+			"f": () => [
+				sprite("Pbox"),
+				area(),
+				pos(),
+				color(256,256,256),
+				z(1),
+				"pgoal",
+				{
+					Trigger: false
+				},
+			],
+			" ": () => [
+				sprite("airb"),
+				area(),
+				pos(),
+				color(136,139,141,),
+				z(0),
+				"air",
+			],
+		});
+	}
 
 	const Player = get("player")[0]
 	const PGoal = get("pgoal")[0]
@@ -159,7 +292,12 @@ scene("game", ({levelid}) => {
 		Player.move(movementp, 0);
 		Player.moveTo(Math.round(Player.pos.x / 100) * 100, Math.round(Player.pos.y / 100) * 100);
 	})
-
+	onKeyPress("r", () => {
+		go("game", {
+			levelid: levelid,
+			bonus: bonus,
+		})
+	})
 
 	var counter = 0;
 	onCollide("player","Moveable",(obj1,obj2) => {
@@ -239,66 +377,103 @@ scene("game", ({levelid}) => {
 			}
 		}
 	})
-	const goals = get("goal");
-	const boxes = get("boxx");
-	var randomfix;//idk why this fixes the code
 	loop(0.5, () => {
-		const checks = [];
-		for(var i = 0; i<goals.length; i++){
-			checks.push(false);
-		}
+		if(get("goal").length>0){
+			const goals = get("goal");
+			const boxes = get("boxx");
+			var randomfix;//idk why this fixes the code
+			const checks = [];
 			for(var i = 0; i<goals.length; i++){
-				for(var o = 0; o<boxes.length; o++){
-					randomfix = goals[i];
-					if(randomfix.isColliding(boxes[o])){
-						checks[i] = true;
+				checks.push(false);
+			}
+				for(var i = 0; i<goals.length; i++){
+					for(var o = 0; o<boxes.length; o++){
+						randomfix = goals[i];
+						if(randomfix.isColliding(boxes[o])){
+							checks[i] = true;
+						}
 					}
 				}
+			var finalcheck = Player.isColliding(PGoal);
+			for(var i = 0; i<checks.length; i++){
+				finalcheck = finalcheck && checks[i];
 			}
-		//debug.log(checks);
-		var finalcheck = Player.isColliding(PGoal);
-		for(var i = 0; i<checks.length; i++){
-			finalcheck = finalcheck && checks[i];
-		}
-		if(finalcheck){
-			debug.log("win");
-			if (levelid < LEVELS.length - 1) {
-				// If there's a next level, go() to the same scene but load the next level
-				go("game", {
-					levelIdx: levelid + 1,
-				})
-			} else {
-				// Otherwise we have reached the end of game, go to "win" scene!
-				go("win", {})
+			if(finalcheck){
+				play("score");
+				//debug.log("win");
+				if (levelid < LEVELS.length - 1 && bonus == false) {
+					// If there's a next level, go() to the same scene but load the next level
+					go("game", {
+						levelid: levelid + 1,
+						bonus: false,
+					})
+				} else {
+					// Otherwise we have reached the end of game, go to "win" scene!
+					go("win", {})
+				}
+			}
+		}else{
+			play("score");
+			if(Player.isColliding(PGoal)){
+				if (levelid < LEVELS.length - 1) {
+					// If there's a next level, go() to the same scene but load the next level
+					go("game", {
+						levelid: levelid + 1,
+						bonus: false,
+					})
+				} else {
+					// Otherwise we have reached the end of game, go to "win" scene!
+					go("win", {})
+				}
 			}
 		}
 	})
 })
 
+function bonussrt(){
+	go("game", {
+		levelid: 0,
+		bonus: true,
+	})
+}
+
+function srtscr(){
+	go("startscr", {
+		levelid: 0,
+	})
+}
+
+
 scene("startscr", ({levelid}) => {
 	function lvlsrt(){
 		go("game", {
-			levelIdx: 0,
+			levelid: 0,
+			bonus: false,
 		})
 	}
-	addButton("start", center(), lvlsrt);
+	addButton("Start", centerOffSet(0, -200), lvlsrt);
+	add([sprite("rules"), pos(centerOffSet(0, 350)), origin("center"), scale(2)])
 })
 
 scene("win", ({levelid}) => {
 	function lvlsrt(){
 		go("game", {
-			levelIdx: 0,
+			levelid: 0,
+			bonus: false,
 		})
 	}
-	addButton("start", center(), lvlsrt);
+	add([text("You Win"), pos(centerOffSet(0, -100)), origin("center"), scale(2)])
+	addButton("Play Again?", center(), lvlsrt);
+	addButton("Bonus?", centerOffSet(0, 100), bonussrt);
+	addButton("Title Screen", centerOffSet(0, 200), srtscr);
 })
 
 function start() {
 	// Start with the "game" scene, with initial parameters
 	
 	go("startscr", {
-		levelIdx: 0,
+		levelid: 0,
 	})
 }
 
-start("game", { level: 0})
+start("startscr", { levelid: 0})
